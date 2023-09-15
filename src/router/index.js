cb1e727917cee5eb5f5552e24f08f0b505059742
tarsweb <58361241+tarsweb@users.noreply.github.com>
2023-09-16 00:00:00 +0300

    add route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,13 +11,34 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/movies',
+      name: 'movies',
+      component : () => import ('../views/MoviesView.vue')
+    },
+    {
+      path: '/movies/:moviesId',
+      component : () => import ('../views/MoviesView.vue'),
+      children: [
+        {
+          path: '/cast', //!!!!!!
+          component : () => import('../views/AboutView.vue')
+        },
+        {
+          path: '/reviews', //!!!!!!
+          component : () => import('../views/AboutView.vue')
+        }
+      ]
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    // and finally the default route, when none of the above matches:
+    { path: "/:pathMatch(.*)*", component: NotFoundView }
   ]
 })
 
