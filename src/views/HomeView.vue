694ae96a4e5dc442810a9d071bcc341243c750af
tarsweb <58361241+tarsweb@users.noreply.github.com>
2023-09-16 21:57:09 +0300

    refactiring

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,38 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+//import TheWelcome from '../components/TheWelcome.vue'
+import MoviesCardSet from '@/components/MoviesCardSet.vue';
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+
+const apiKey=  "bb9be7856d820d280efdc8865f07d5b2"
+const movies = ref([])
+
+const route = useRoute()
+const page = route.query.page ?? 1
+
+;
+
+async function getTrendingMovies(category) {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/trending/movie/${category}?api_key=${apiKey}&page=${page}`
+  );
+  const data = await response.json();
+  console.log(data);
+  movies.value = data.results;
+}
+
+onMounted(() => 
+    getTrendingMovies('week')
+)
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <div v-if="movies.length > 0 ">
+      <MoviesCardSet :movies="movies"/> 
+    </div>
   </main>
 </template>
