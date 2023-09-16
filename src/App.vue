c6b88dc5d621ae02db927d4c67daa14cc4cb6c45
tarsweb <58361241+tarsweb@users.noreply.github.com>
2023-09-16 21:53:06 +0300

    add and change style

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+const navItems = [
+  { href: '/', text: 'Home' },
+  { href: '/movies', text: 'Movies' },
+  { href: '/about', text: 'About' }
+]
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="25" height="25" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!-- <HelloWorld msg="You did it!" /> -->
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/movies">Movies</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+      <nav class="navlist">
+        <RouterLink v-for="item in navItems" :to="item.href">{{ item.text }}</RouterLink>
       </nav>
     </div>
   </header>
-  
+
   <RouterView />
-  
 </template>
 
 <style scoped>
+.router-link-active {
+  text-decoration-line: underline;
+}
+header{
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    gap: 10px;
+}
+.wrapper{
+  width: 100%;
+  display: flex;
+  gap: 10px;
+}
+
+.navlist{
+  width: 100%;
+  display: flex;
+  gap: 10px;
+}
+
+.navlist :last-child {
+  margin-left: auto;
+} 
 </style>
