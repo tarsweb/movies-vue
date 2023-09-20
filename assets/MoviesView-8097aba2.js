139fed197bc4cb8d8ee8b474ee3edda7fb53a725
tarsweb <tarsweb@users.noreply.github.com>
2023-09-20 15:00:24 +0000

    deploy: 5be511ea83468d99aaebfe296d4638179631a692

---
+++ assets/MoviesView-8097aba2.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as o,a as c}from"./index-966d328d.js";const t={},a={class:"movies"},n=c("h1",null,"This is an movies page",-1),_=[n];function i(r,d){return s(),o("div",a,_)}const f=e(t,[["render",i]]);export{f as default};
